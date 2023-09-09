7fecc684ac63c0757edde4110bfdd5c7acf5929a
preciousnjukia <preciousnjukia@gmail.com>
2023-09-09 16:59:35 +0300

    Coded along

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,11 +17,18 @@ function Login({ setIsLoggedIn }) {
 
   function handleSubmit(e) {
     e.preventDefault();
-
-    setIsLoggedIn(true);
-
-    // after logging the user in, redirect to the home page!
-    history.push("/");
+    fetch("http://localhost:3001/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((r) => r.json())
+      .then((user) => {
+        setIsLoggedIn(user);
+        history.push("/");
+      });
   }
 
   return (
